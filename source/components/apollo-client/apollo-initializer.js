63fdc0aa6d25749b115390c292b92024b103d1a6
Jaaouani Oussama <j440u4ni@gmail.com>
2019-05-27 18:44:50 +0200

    setup the website in iMac
    setup the website in iMac

--- source/components/apollo-client/apollo-initializer.js
+++ source/components/apollo-client/apollo-initializer.js
@@ -5,7 +5,7 @@ import fetch from 'isomorphic-fetch';
 
 if(!process.browser) { global.fetch = fetch; }
 
-const linkGuest = new HttpLink({  uri: 'http://localhost:3000/v1/graphql-account', fetch: fetch });
+const linkGuest = new HttpLink({  uri: 'http://10.188.37.107:3000/v1/graphql-first-instance/BtL7NQwOt0R7psYw1Fyx', fetch: fetch });
 const linkMain = ApolloLink.from([linkGuest]);
 const apolloClientGuest = new ApolloClient({ uri: linkMain });
 
